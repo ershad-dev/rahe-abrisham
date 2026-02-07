c2ba696ba91d5132ae067a52a024c89ab86758b7
Ershuad <khademiershad@gmail.com>
2026-02-07 04:42:23 +0330

    remove font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,11 +11,7 @@ export default <Partial<Config>>{
       colors: {
         'rahe-abrisham-primary': '#00A3A3',
         'rahe-abrisham-light': '#F0F8F8',
-      },
-      fontFamily: {
-        // اگر بعداً فونت فارسی اضافه کردی
-       inter: ['Inter', 'system-ui', 'sans-serif'],
-      },
+      }
     },
   },
 
